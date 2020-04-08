3ad3f4497ccb990450673bcfdb7e8937e8bfd04c
nhatanhdoan1505 <58335498+nhatanhdoan1505@users.noreply.github.com>
2020-04-08 16:13:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ mongoose.connect('mongodb+srv://doannhatanh:dhoPQ3lemwLNCtvG@cluster0-g513c.mong
     }
 });
 
-app.listen(3000, function () {
-    console.log('Connect successful !');
+const listener = app.listen(process.env.PORT || 8000, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
 
 
